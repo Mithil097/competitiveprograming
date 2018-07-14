c3e3c8c19b028889e36ecf0abe215bc6beeb74ca
Mithil097 <31200175+Mithil097@users.noreply.github.com>
2018-07-14 09:43:28 +0530

    Add files via upload

---
+++ competitiveprogramming/week3/day2/egg_problem.py
@@ -0,0 +1,21 @@
+def egg(n,k):
+
+    # Write the body of your function here
+    if(k==0 or k==1):
+        return k
+    if(n==1):
+        return k
+    min=100
+    i=1
+    res=k+1
+    for i in range(k):
+       res=max(egg(n-1,i-1),egg(n,k-i))
+       if(res<min):
+           min=res
+
+    return min+1
+
+# Run your function through some test cases here
+# Remember: debugging is half the battle!
+f=egg(2,11)
+print(f)
